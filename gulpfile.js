1ef5223e8b816af101e01d7627409e1d9ffbe20e
borjomeeee <somanfrom58@gmail.com>
2019-10-04 20:40:04 +0300

    Add build js & watch js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,30 @@
-function init(cb) 
+const { src, dest, watch, parallel } = require('gulp');
+const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+
+function js(cb) 
 {
-    console.log('Hello, world!');
+    src(['src/js/*js', '!node_modules/**'])
+        .pipe(concat('index.js'))
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(dest('dest/js'))
+        .pipe(uglify())
+        .pipe(rename((path) => path.extname = '.min.js'))
+        .pipe(dest('dest/js'))
+
     cb();
 }
 
-exports.default = init;
+function init(cb) 
+{
+    watch('src/js/*.js', js);
+    
+    cb();
+}
+
+exports.default = parallel(init, js);
 
