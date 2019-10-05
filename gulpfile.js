7998d62e84e623635be20a07825b1015bea39538
borjomeeee <somanfrom58@gmail.com>
2019-10-05 21:23:26 +0300

    angularjsDemo

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 const { src, dest, watch, parallel, series } = require('gulp');
 const concat = require('gulp-concat');
-const babel = require('gulp-babel');
 const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
 
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+
 const browserSync = require('browser-sync').create();
+
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+
+const browserify = require("browserify");
+
+const assets = 'dest/assets/';
 
 function server(cb)
 {
@@ -13,42 +22,66 @@ function server(cb)
             baseDir: './dest'
         }
     });
+    
+    watch('src/page/*.html', html);
+    watch('src/page/*.scss', css);
+    watch('src/page/index.js', js);
+
+    cb();
+}
+
+function html(cb)
+{
+    src('src/page/*.html')
+        .pipe(dest('dest'))
+        .pipe(browserSync.reload({stream:true}));
 
     cb();
 }
 
 function cssLib(cb)
 {
-    src(['src/js/scripts/*.js', '!node_modules/**', '!src/js/lib/*.js'])
-        .pipe(uglify())
-        .pipe(dest('dest/js'));
+    src('src/assets/css/libs/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest(`${assets}styles`));
+
+    cb();
+}
+
+function css(cb)
+{
+    src('src/page/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('style.css'))
+        .pipe(dest(`${assets}styles`))
+        .pipe(browserSync.reload({stream:true}));
 
     cb();
 }
 
 function jsLib(cb)
 {
-    src(['src/js/scripts/*.js', '!node_modules/**', '!src/js/lib/*.js'])
-        .pipe(uglify())
-        .pipe(dest('dest/js'));
+    src(['src/assets/libs/*.js'])
+        .pipe(concat('common.min.js'))
+        .pipe(uglify().on('error', console.error))
+        .pipe(dest(`${assets}scripts`));
 
     cb();
 }
 
 function js(cb) 
 {
-    src(['src/js/scripts/*.js', '!node_modules/**', '!src/js/lib/*.js'])
-        .pipe(concat('index.js'))
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(dest('dest/js'))
-        .pipe(uglify())
-        .pipe(rename((path) => path.extname = '.min.js'))
-        .pipe(dest('dest/js'));
+    browserify('src/page/index.js')
+        .transform("babelify", {presets: ["@babel/preset-env", "@babel/preset-react"]})
+        .bundle()
+        .pipe(source("index.js"))
+        .pipe(rename({ extname: '.min.js' }))
+        .pipe(buffer())
+        .pipe(uglify().on('error', console.error))
+        .pipe(dest(`${assets}scripts`))
+        .pipe(browserSync.reload({stream:true}));
 
     cb();
 }
 
-exports.default = series(parallel(js, jsLib, css, cssLib), server);
-
+exports.default = series(parallel(js, jsLib, css, cssLib, html), server);
